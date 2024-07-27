259939e6a448bcfe2b1b1740d1ea9b0196c6296f
Wesley Sales Ferreira <137358064+lesleysales@users.noreply.github.com>
2024-07-27 17:27:13 -0300

    Atualização Portfólio (27.07.24)

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form, .portfolio-others', { origin: 'bottom' });
 
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 
@@ -60,7 +60,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /* ====== Typed JS ====== */
 
 const typed = new Typed ('.multiple-text',{
-    strings: ['Desenvolvedor', 'UX Designer', 'Administrador de Dados'],
+    strings: ['Desenvolvedor', 'Designer', 'Analista de Dados'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
